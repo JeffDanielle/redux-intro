ea143aac147a9c88e8caf743c4caa4d1c8b1c3a2
JeffDanielle <bayotjeff@gmail.com>
2024-04-05 17:00:09 +0800

    274. Creating the Store With RTK -5min

---
+++ src/store-v2.js
@@ -0,0 +1,20 @@
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { thunk } from "redux-thunk";
+import { composeWithDevTools } from "@redux-devtools/extension";
+import accountReducer from "./features/accounts/accountSlice";
+import customerReducer from "./features/customers/customerSlice";
+
+// Combine reducers essential for redux if we have multiple reducers for multiple states
+const rootReducer = combineReducers({
+    account: accountReducer,
+    customer: customerReducer
+})
+
+// Create store for redux
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+
+export default store;
+
+
+
--- src/store.js
+++ src/store.js
@@ -1,17 +1,16 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
-import { thunk } from "redux-thunk";
-import { composeWithDevTools } from "@redux-devtools/extension";
+
+import { configureStore } from "@reduxjs/toolkit";
+
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
 
-// Combine reducers essential for redux if we have multiple reducers for multiple states
-const rootReducer = combineReducers({
-    account: accountReducer,
-    customer: customerReducer
+// Modern way of fetching the state in redux !! CHECK V2 FOR REFERENCE !!
+const store = configureStore({
+    reducer: {
+        account: accountReducer,
+        customer: customerReducer
+    }
 })
-
-// Create store for redux
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 export default store;
 
